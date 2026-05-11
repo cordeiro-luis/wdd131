aa2f8fe5fb7082b4fe098c8f61201751529f01a8
cordeiro-luis <luis@cordeiro.sytes.net>
2026-05-11 23:35:27 +0100

    W04 Ponder: Responsive Menu

---
+++ ponder/responsive.js
@@ -0,0 +1,9 @@
+const btn = document.querySelector(".menu-btn");
+const menu = document.querySelector("nav");
+
+btn.addEventListener("click", ToggleMenu);
+
+function ToggleMenu() {
+    menu.classList.toggle("hide");
+    btn.classList.toggle("change");
+}
