5f56b3e486b01bfaf4ff2d45f4ecb4ce8163b5e6
cordeiro-luis <luis@cordeiro.sytes.net>
2026-05-26 08:02:20 +0100

    W06 Prove: Build a Blog part 2

---
+++ blog/js/script.js
@@ -0,0 +1,76 @@
+
+const articles = [
+    {
+        id: 1,
+        title: 'Septimus Heap Book One: Magyk',
+        date: 'July 5, 2022',
+        description:
+            'If you enjoy stories about seventh sons of seventh sons and magyk this is the book for you.',
+        imgSrc: 'images/magkycover2.jpg',
+        imgAlt: 'Book cover for Septimus Heap 1',
+        ages: '10-14',
+        genre: 'Fantasy',
+        stars: '⭐⭐⭐⭐'
+    },
+    {
+        id: 2,
+        title: 'Magnus Chase Book One: Sword of Summer',
+        date: 'December 12, 2021',
+        description:
+            'The anticipated new novel by Rick Riordan. After Greek mythology (Percy Jackson), Greek/Roman (Heroes of Olympus), and Egyptian (Kane Chronicles), Rick decides to try his hand with Norse Mythology, and the end result is good.',
+        imgSrc:
+            'images/swordsummer.jpg',
+        imgAlt: 'Book cover for Magnus Chase 1',
+        ages: '12-16',
+        genre: 'Fantasy',
+        stars: '⭐⭐⭐⭐'
+    },
+    {
+        id: 3,
+        title: "Belgariad Book One: Pawn of Prophecy",
+        date: "Feb 12, 2022",
+        description:
+            "A fierce dispute among the Gods and the theft of a powerful Orb leaves the World divided into five kingdoms. Young Garion, with his 'Aunt Pol' and an elderly man calling himself Wolf --a father and daughter granted near-immortality by one of the Gods -- set out on a complex mission.",
+        imgSrc:
+            "images/pawnprophecy.jpg",
+        imgAlt: "Book cover for Pawn of Prophecy",
+        ages: "12-16",
+        genre: "Fantasy",
+        stars: "⭐⭐⭐⭐⭐"
+    }
+];
+
+const bookList = document.querySelector('#book-list');
+
+
+articles.forEach(article => {
+
+    const articleElement = document.createElement('article');
+    articleElement.className = 'blog-post';
+
+    let html = `<div class="post-meta">
+                <p class="date">${article.date}</p>
+                <p class="age-range">${article.ages}</p>
+                <p class="genre">${article.genre}</p>
+                <p class="rating" aria-label="Rating: ${article.stars.length} out of 5 stars" role="img">${article.stars}</p>
+            </div>
+
+            <div class="post-content">
+                <h2>${article.title}</h2>
+                <img src="${article.imgSrc}" alt="${article.imgAlt}">
+                <p>
+                    ${article.description}
+                </p>
+            </div>`
+
+    articleElement.innerHTML = html;
+    bookList.appendChild(articleElement);
+
+});
+
+
+// On part 1 of this assignment I used sections inside the single article, because I thought that would be
+// semantically correct. However, is it correct if instead I have multiple "books"
+// on the same page? Not really.
+// I switched these sections to div on part 2, and made each book be on its own article,
+// and I think that is more semantically correct now.
